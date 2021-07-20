00f1122edeb0f3115f4bd587d6718da41934bf6f
Zaid Afaneh <zaid@realblocks.com>
2021-07-20 16:25:16 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from os import system
+import sys
+from time import sleep
+from time import time
+
+def make_call():
+    from twilio.rest import Client
+    import os
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+    client = Client(account_sid, auth_token)
+
+    call = client.calls.create(
+        twiml='<Response><Say>Hurry Up</Say></Response>',
+        # to='+962790810526',
+        to=os.environ['MY_PHONE'],
+        from_=os.environ['TWILIO_PHONE']
+    )
+    print(call.sid)
+def delay():
+    from random import randint
+    from random import uniform
+    sleep(uniform(1, 10))
+def manasa():
+    from time import time
+    from datetime import datetime
+    from selenium import webdriver
+    from selenium.webdriver.support.ui import Select
+    from selenium.webdriver.firefox.options import Options
+
+    print ('[%s] Started Script' %(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
+    link = 'https://www.gateway2jordan.gov.jo/landplatform/'
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
+    driver.get(link)
+    with open("log.log" , "w+") as f:
+        while True: 
+            print ('[%s] Page Loaded' %(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")),file=f)
+            options = Select(driver.find_element_by_xpath('//*[@id="ddlCrossingpoint"]')).options
+            start = time()
+            print (f'[%s] {[ option.text for option in options]}' %(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")),file=f)
+            for option in options:
+                words = option.text.split()
+                for word in words: 
+                    if 'king' == word.lower():
+                        print ('[%s] Making Call' %(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")),file=f)
+                        make_call()
+                        break
+
+            if len(options) != 5:
+                print ('[%s] Making Call' %(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")),file=f)
+                make_call()
+                break
+            print ('[%s] Didn\'t Open Yet' %(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")), flush=True,file=f)
+            delay()
+            driver.refresh()
+            system("clear")
+
+
+def main():
+    manasa()
+
+if __name__ == "__main__":
+    main()
+
+
